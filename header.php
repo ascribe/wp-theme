26a51ef1102614c07f54cd26ea7496d2992b56fa
Matthias Kretschmann <m@kretschmann.io>
2016-01-16 13:18:29 +0100

    cleanup
    - remove modernizr
    - improved TypeKit script
    - remove unused normalize copy

--- header.php
+++ header.php
@@ -20,12 +20,9 @@ if ( is_category() || is_tag() ) {
 }
 
 ?>
+
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js  ie6 oldie" lang="en" itemscope itemtype="https://schema.org/Organization"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js  ie7 oldie" lang="en" itemscope itemtype="https://schema.org/Organization"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js  ie8 oldie" lang="en" itemscope itemtype="https://schema.org/Organization"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js " lang="en" itemscope itemtype="https://schema.org/Organization" xmlns="http://www.w3.org/1999/html"> <!--<![endif]-->
+<html class="no-js " lang="en" itemscope itemtype="https://schema.org/Organization" xmlns="http://www.w3.org/1999/html">
 
 <head>
     <meta charset="utf-8">
@@ -72,20 +69,27 @@ if ( is_category() || is_tag() ) {
     <meta name="msapplication-config" content="<?php echo WPTHEME_TEMPLATE_URL; ?>/images/ico/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-    <script src="https://use.typekit.net/gma2yhj.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <!-- Typekit -->
+    <script>
+    (function(d) {
+        var config = {
+            kitId: 'gma2yhj',
+            scriptTimeout: 3000
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
+    </script>
 
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-60614729-2', 'auto');
-          ga('send', 'pageview');
+      ga('create', 'UA-60614729-2', 'auto');
+      ga('send', 'pageview');
 
-        </script>
+    </script>
 
     <?php wp_head(); ?>
 </head>
