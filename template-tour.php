bb04669b7a36afddf944ed2455073a52a0b26889
Matthias Kretschmann <m@kretschmann.io>
2016-03-03 15:39:32 +0100

    fix front page logo too, #40

--- template-tour.php
+++ template-tour.php
@@ -16,8 +16,21 @@ $controller     = new Controller();
 <header class="hero" style="background-image:url(<?php echo $bgImage; ?>)">
     <div class="sticky">
         <div class="row">
-            <a href="<?php echo get_bloginfo('wpurl');?>"><img src="<?php echo WPTHEME_TEMPLATE_URL; ?>/images/logo/logo-white.png" class="logo phone-and-up"></a>
-            <a href="<?php echo get_bloginfo('wpurl');?>"><img src="<?php echo WPTHEME_TEMPLATE_URL; ?>/images/logo/ascribeicon-white.svg" class="logo phone-only"></a>
+
+            <a href="<?php echo get_bloginfo('wpurl');?>">
+                <img
+                    src="<?php echo WPTHEME_TEMPLATE_URL; ?>/images/logo/logo-white.png"
+                    srcset="<?php echo WPTHEME_TEMPLATE_URL; ?>/images/logo/logo-white.png 1x, <?php echo WPTHEME_TEMPLATE_URL; ?>/images/logo/logo-white@2x.png 2x"
+                    alt="ascribe logo"
+                    class="logo phone-and-up">
+            </a>
+            <a href="<?php echo get_bloginfo('wpurl');?>">
+                <img
+                    src="<?php echo WPTHEME_TEMPLATE_URL; ?>/images/logo/ascribeicon-white.svg"
+                    alt="ascribe logo"
+                    class="logo phone-only">
+            </a>
+
             <div class="app-links">
                 <a href="<?php echo $signInLink; ?>">Log In</a> / <a href="<?php echo $signUpLink; ?>">Sign Up</a>
 
