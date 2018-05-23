5c892f8234de501cbb5dfb9072dc19e965ad736e
Matthias Kretschmann <m@kretschmann.io>
2018-05-23 13:23:34 +0200

    switch to loading Typekit without js
    * results in no Adobe cookies being set

--- ascribe/header.php
+++ ascribe/header.php
@@ -66,15 +66,7 @@ if ( is_category() || is_tag() ) {
     <meta name="theme-color" content="#ffffff">
 
     <!-- Typekit -->
-    <script>
-    (function(d) {
-        var config = {
-            kitId: 'gma2yhj',
-            scriptTimeout: 3000
-        },
-        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-        })(document);
-    </script>
+    <link rel="stylesheet" href="https://use.typekit.net/gma2yhj.css">
 
     <?php wp_head(); ?>
 </head>
